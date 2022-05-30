fbea981fc3720fae8c579426970fb0cd7418cc7f
Rajat <mail.rajatdalmia@gmail.com>
2022-05-30 12:03:00 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GitBankingPrj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Git Banking....");
+        }
+    }
+}
